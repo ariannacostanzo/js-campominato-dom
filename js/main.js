20a3a8f13b8c83ff1bd5ed6fa355a1e2f5871577
ariannacostanzo <costanzo.arianna@outlook.it>
2024-01-11 14:00:11 +0100

    fixed cell dimension change

--- js/main.js
+++ js/main.js
@@ -3,11 +3,15 @@
 // Todo Creo le celle secondo le dimensioni della griglia
 // Todo Cambio la dimensione delle celle in base al select
 
+// Raccolgo gli elementi della pagina che mi servono
 const gridElement = document.getElementById('grid');
 const form = document.querySelector('form');
 const difficultyLevelElement = document.querySelector('select');
 const button = document.querySelector('button');
 
+//---------------------
+//FUNZIONI
+//---------------------
 
 const logSomething = (something) => console.log(something);
 
@@ -22,14 +26,12 @@ const createCell = (content) => {
     return newCell;
 }
 
-
-//tutta questa logica andrebbe in una funzione a parte chiamata startGame
-form.addEventListener('submit', (e) => {
+const startGame = (e) => {
     // *impedisco il reload al submit e svuoto la griglia 
     e.preventDefault();
     gridElement.innerHTML = '';
 
-    //Cambio il testo del button
+    //*Cambio il testo del button
     button.innerText = 'Ricomincia'
 
     // *Ottengo le dimensioni di rows e cols in base al select
@@ -37,29 +39,28 @@ form.addEventListener('submit', (e) => {
     logSomething(difficultyLevel);
     let rows = 10;
     let cols = 10;
-    let difficultyLevelCode = 'easy';
 
     switch(difficultyLevel) {
         case 'normal':
             rows = 9;
             cols = 9;
-            difficultyLevelCode = 'normal';
             break;
         case 'hard': 
             rows = 7;
             cols = 7;
-            difficultyLevelCode = 'hard';
             break;
     }
 
     const cellQuantity = rows * cols;
 
+    //*cambio la dimensione delle celle nel css
+    const root = document.querySelector(':root');
+    root.style.setProperty('--rows-per-cols', cols);
+
     //* creo tante celle quante rows * cols
     for (let i = 1; i <= cellQuantity; i++) {
         const cell = createCell(i);
 
-        //* aggiungo la classe css per regolare la dimensione delle celle
-        cell.classList.add(difficultyLevelCode);
 
         //* gestisco il cambio colore al click delle celle
         cell.addEventListener('click', () => {
@@ -69,6 +70,36 @@ form.addEventListener('submit', (e) => {
 
         gridElement.appendChild(cell);
     }
-    
-});
+};
 
+
+
+//Inizio il gioco con il submit del form
+form.addEventListener('submit', startGame);
+
+
+
+//le celle vanno cliccate solo una volta ed il colore non è toggle
+
+//! milestone 1
+//serve un contatore che tiene i punti dell'utente ogni volta che clicca una cella che non è stata cliccata prima
+//mettere il punteggio in pagina, nell'header
+
+//! milestone 2
+//generiamo 16 numeri casuali (tutti diversi) nel range della difficoltà prescelta
+//serve il totale bombe ed il massimo punteggio; serve una funzione che genera le bombe
+//la funzione avrà come paramentri il numero di bombe ed il numero di celle in cui si dispongono
+//dentro ci sarà un array che va riempito con 16 numeri casuali ma tutti diversi
+//serve un ciclo while che riempie l'array finche avrà 16 numeri diversi
+
+//! milestone 3
+//quando l'utente clicca su una cella, verifichiamo se la cella che ha premuto è una delle bombe,
+//controllando se il numero di cella è nell'array delle bombe, se si la cella diventa rossa, raccogliamo iol punteggio e la
+//partita termina, se no la cella è blu ed incrementa il punteggio, attenzione a fare una logic game over con flag
+//la parte in cui controllo se ha vinto o perso e gli dico il punteggio lo metto in una funzione
+
+//! bonus 
+//alla fine scopri tutte le bombe e poi posso fare in modo che il gioco ricominci
+//richiamando la funzione startGame() che deve avere tutto il codice
+//per rivelare tutte le celle devo andarle a prendere tutte con queryselectorall e poi
+//e dare classlist e fare girare in un ciclo for(let cell of cells) {cell.classlist.add('clicked)}
